f72d602866cad0d555cbf48e343593a497fd3bcf
KimsanboyevShoxidbek2000 <76057864+KimsanboyevShoxidbek2000@users.noreply.github.com>
2023-08-29 13:03:07 +0500

    fhfh

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,19 @@
 import React from "react";
-
 // components
-import Products from "./components/products/Products";
-import Services from "./components/services/Services";
-import About from "./components/about/About";
-import News from "./components/news/News";
-import Partners from "./components/partners/Partners";
+// import Products from "./components/products/Products";
+// import Services from "./components/services/Services";
+// import About from "./components/about/About";
+// import News from "./components/news/News";
+// import Partners from "./components/partners/Partners";
 const App = () => {
   return (
     <div>
-      <Products />
+      {/* <Products />
       <Services />
       <About />
       <News />
-      <Partners />
+      <Partners /> */}
+     
     </div>
   );
 };
---
+++ src/assets/images/index.js
@@ -0,0 +1,26 @@
+const IMAGES = {
+  logo: require('./logo 1.png'),
+  printer: require ('./slide.png'),
+  productimg1: require('./endeavor-large 1.png'),
+  productimg2: require('./img6 1.png'),
+  productimg3: require('./gallery_cs300_1 1.png'),
+  productimg4: require('./754-1 1.png'),
+  productimg5: require('./img5 1.png'),
+  productimg6: require('./pacemaker-advisa-mrishtsh222 1.png'),
+  uslugimg1: require('./Frame 6.png'),
+  uslugimg2: require('./Frame 7.png'),
+  uslugimg3: require('./Frame 8.png'),
+  newimg: require('./20180420_101750 1.png'),
+  sponsorimg1: require("./aesculap-b-braun1 1.png"),
+  sponsorimg2: require("./asp1 1.png"),
+  sponsorimg3: require("./bd1 1.png"),
+  sponsorimg4: require("./biosense1 1.png"),
+  sponsorimg5: require("./cordis1 1.png"),
+  sponsorimg6: require("./eth 1.png"),
+  sponsorimg7: require("./ethicon-endo-surgery1 1.png"),
+
+
+
+}
+
+export default IMAGES
